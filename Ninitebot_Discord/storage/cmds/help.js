efca80e60028efc061519558049f9eb0dfd8d6a1
Josh Bennett <theninitegamer@gmail.com>
2018-09-26 17:56:32 -0400

    CLEANING UP THE GITHUB

--- Ninitebot_Discord/storage/cmds/help.js
+++
@@ -1,9 +0,0 @@
-let Discord = module.require(`discord.js`)
-const fs = module.require(`fs`)
-module.exports.run = async (client, message, args) => {
-    return message.author.send("Here's the commands !purge <amount>, !meme, !johnny, !dan, !quote, !ping")
-}
-
-module.exports.help = {
-    name: "help"
-}
