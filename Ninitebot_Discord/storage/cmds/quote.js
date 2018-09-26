2018215b792cd04529b0cfc7f475738fdefa8b4e
Josh Bennett <theninitegamer@gmail.com>
2018-09-26 17:56:51 -0400

    Delete quote.js

--- Ninitebot_Discord/storage/cmds/quote.js
+++
@@ -1,9 +0,0 @@
-let Discord = module.require(`discord.js`)
-const fs = module.require(`fs`)
-module.exports.run = async (client, message, args) => {
-    return message.channel.send(`Pong!`)
-}
-
-module.exports.help = {
-    name: "quote"
-}
